d9a4554e1f90b838d80aead87d24c8c75208816b
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2012-02-09 13:51:40 +0000

    make PythonParticipant working

--- tools/worker.rb
+++ tools/worker.rb
@@ -36,7 +36,7 @@ class FakeParticipant
     include Ruote::LocalParticipant
     def consume(workitem)
         puts workitem.inspect
-        puts "workitem consumed"
+        puts "workitem consumed\n" + workitem.inspect
         reply_to_engine(workitem)
     end
 end
@@ -47,7 +47,7 @@ class PythonParticipant
     def consume(workitem)
         wi_str = workitem.as_json
         out_wi_str = ""
-        IO.popen("pyparticipant", 'w+') do |subprocess|
+        IO.popen("pyparticipant #{workitem.params['name']}", 'w+') do |subprocess|
             subprocess.write(wi_str)
             subprocess.close_write()
             subprocess.read.split("\n").each do |line|
@@ -57,8 +57,13 @@ class PythonParticipant
                 end
             end
         end
-        new_wi = Ruote::Workitem.from_json(out_wi_str)
-        reply_to_engine(new_wi)
+        if out_wi_str != "" then
+            new_wi = Ruote::Workitem.from_json(out_wi_str)
+            reply_to_engine(new_wi)
+        else
+            puts "Wrapper haven't returned workitem"
+            reply_to_engine(workitem)
+        end
     rescue
         puts "problem occured: " + $!
     end
@@ -66,15 +71,15 @@ class PythonParticipant
 end
 
 $engine.register_participant :fake1, FakeParticipant
-$engine.register_participant :pyfake, PythonParticipant
+$engine.register_participant :python, PythonParticipant
 $engine.register_participant :hardworker, RuoteAMQP::ParticipantProxy, :queue => 'taskqueue'
 
 pdef = Ruote.process_definition do
-    pyfake
-    fake1
+    python :name => 'branch_repo'
+    fake1 :p1 => 'gggght'
 end
 
-wfid = $engine.launch(pdef, :f1 => "qwerty")
+wfid = $engine.launch(pdef, :repo => "test_repo", :user => 'vasya')
 
 puts "Engine running"
 $engine.join()
