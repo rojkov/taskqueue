327d900c64e3d011e6579defc3624a8fa0a36f14
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2012-02-20 11:55:01 +0000

    bump up version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "taskqueue",
-    version = "0.1.0",
+    version = "0.2.0",
     author = "Dmitry Rozhkov",
     author_email = "dmitry.rojkov@gmail.com",
     description = ("Simple task queue with round robin load balancing."),
