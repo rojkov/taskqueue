46af66bda92c4a37469289bb4035cf3242ed6020
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2012-03-03 12:53:31 +0000

    allow overriding of default content type and map in dispatcher

--- taskqueue/dispatcher.py
+++ taskqueue/dispatcher.py
@@ -8,34 +8,12 @@ import pika
 import json
 
 from taskqueue.daemonlib import Daemon
-from taskqueue.workitem import get_workitem, WorkitemError
+from taskqueue.workitem import get_workitem, WorkitemError, DEFAULT_CONTENT_TYPE
 
 LOG = logging.getLogger(__name__)
 
-def handle_delivery(channel, method, header, body):
-    """Handle delivery from WFE."""
-    LOG.debug("Method: %r" % method)
-    LOG.debug("Header: %r" % header)
-    LOG.debug("Body: %r" % body)
-
-    try:
-        workitem = get_workitem(header, body)
-    except WorkitemError as err:
-        # Report error and accept message
-        LOG.error("%s" % err)
-        channel.basic_ack(method.delivery_tag)
-        return
-
-    worker = workitem.worker_type
-    channel.basic_publish(exchange='',
-                          routing_key='worker_%s' % worker,
-                          body=body,
-                          properties=pika.BasicProperties(
-                              delivery_mode=2,
-                              content_type=header.content_type
-                          ))
-
-    channel.basic_ack(method.delivery_tag)
+# TODO: move the constants to confparser
+SECTION_WORKERS = 'workers'
 
 class Dispatcher(Daemon):
     """Dispatcher daemon"""
@@ -49,17 +27,50 @@ class Dispatcher(Daemon):
         self.connection = None
         super(Dispatcher, self).__init__(config)
 
+    def handle_delivery(self, channel, method, header, body):
+        """Handle delivery from WFE."""
+        LOG.debug("Method: %r" % method)
+        LOG.debug("Header: %r" % header)
+        LOG.debug("Body: %r" % body)
+        settings = dict(self.config.items(SECTION_WORKERS))
+
+        try:
+            workitem = get_workitem(header, body,
+                                    settings.get('workitem_type_map', None),
+                                    settings.get('default_workitem_type',
+                                                 DEFAULT_CONTENT_TYPE))
+        except WorkitemError as err:
+            # Report error and accept message
+            LOG.error("%s" % err)
+            channel.basic_ack(method.delivery_tag)
+            return
+
+        worker = workitem.worker_type
+        channel.basic_publish(exchange='',
+                              routing_key='worker_%s' % worker,
+                              body=body,
+                              properties=pika.BasicProperties(
+                                  delivery_mode=2,
+                                  content_type=header.content_type
+                              ))
+
+        channel.basic_ack(method.delivery_tag)
+
     def run(self):
         """Event cycle."""
 
         LOG.debug("run!")
+
+        if not self.config.has_section(SECTION_WORKERS):
+            self.config.add_section(SECTION_WORKERS)
+
         LOG.debug("create connection")
         self.connection = pika.BlockingConnection(self.amqp_params)
         LOG.debug("dispatcher connected")
         self.channel = self.connection.channel()
         self.channel.queue_declare(queue="taskqueue", durable=True,
                                    exclusive=False, auto_delete=False)
-        self.channel.basic_consume(handle_delivery, queue="taskqueue")
+        self.channel.basic_consume(self.handle_delivery, queue="taskqueue")
         self.channel.start_consuming()
 
     def cleanup(self, signum, frame):
